b7c7b8c0097acc33f8f6cd9469274f60f3e08ec2
PandaQ <767807368@qq.com>
2017-04-25 20:06:03 +0800

    2017-04-25

--- app/src/main/java/com/pandaq/pandaeye/BaseActivity.java
+++ app/src/main/java/com/pandaq/pandaeye/BaseActivity.java
@@ -29,6 +29,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
